6df0c25a62bf52d507427ba6c31062e706bd51d9
coderprasukj <prasukjain0047@gmail.com>
2021-03-12 15:18:09 +0530

    Some Basics Installation

---
+++ models/user.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    email: {
+        type: String,
+        default: ''
+    },
+    gender: {
+        type: String,
+    },
+    timezone: {
+        type: String,
+        default: '',
+    },
+    atcoder: {
+        type: Boolean,
+        default: true,
+    },
+    codechef: {
+        type: Boolean,
+        default: true,
+    },
+    codeforces: {
+        type: Boolean,
+        default: true,
+    },
+    leetcode: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
